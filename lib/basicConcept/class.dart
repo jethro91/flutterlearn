cd86be68318b7c1d85702797974c0102c12e7e3c
jethro91 <jethro@datalinkdigital.com>
2019-11-18 09:29:21 +0800

    [NAMA FITUR] Keterangan

--- lib/basicConcept/class.dart
+++ lib/basicConcept/class.dart
@@ -1,4 +1,20 @@
-class OrangStatic {
-  static String nama = 'Angel';
-  static int umur = 20;
+class Level1 {
+  Level1({
+    this.nama: 'defaultName',
+    this.job: 'novice',
+  });
+  final String nama;
+  final String job;
+
+  hello() {
+    print('Hi nama saya $nama pekerjaan $job');
+  }
+}
+
+void main() {
+  Level1 orang1 = Level1();
+
+  orang1.hello();
+  Level1 orang2 = Level1(nama: 'Erihc', job: 'sniper');
+  orang2.hello();
 }
